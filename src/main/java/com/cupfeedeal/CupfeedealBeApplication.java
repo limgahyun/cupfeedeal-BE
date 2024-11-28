16ad505e477f891d07679da30276b60cffefc336
limgahyun <limgh2002@gmail.com>
2024-11-28 19:46:29 +0900

    git inite

---
+++ src/main/java/com/cupfeedeal/CupfeedealBeApplication.java
@@ -0,0 +1,13 @@
+package com.cupfeedeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupfeedealBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupfeedealBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cupfeedeal/CupfeedealBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cupfeedeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupfeedealBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
