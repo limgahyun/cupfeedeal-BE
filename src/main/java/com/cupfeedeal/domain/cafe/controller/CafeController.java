d84f82384d0a05600b61cccd130895f0a714d94e
limgahyun <limgh2002@gmail.com>
2025-01-08 15:28:49 +0900

    feat: 추천 카페 조회 controller

--- src/main/java/com/cupfeedeal/domain/cafe/controller/CafeController.java
+++ src/main/java/com/cupfeedeal/domain/cafe/controller/CafeController.java
@@ -1,15 +1,20 @@
 package com.cupfeedeal.domain.cafe.controller;
 
 import com.cupfeedeal.domain.cafe.dto.request.CafeCreateRequestDto;
+import com.cupfeedeal.domain.cafe.dto.response.CafeNewOpenListResponseDto;
+import com.cupfeedeal.domain.cafe.dto.response.CafeRecommendationListResponseDto;
 import com.cupfeedeal.domain.cafe.entity.Cafe;
 import com.cupfeedeal.domain.cafe.service.CafeService;
 import com.cupfeedeal.global.common.response.CommonResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Tag(name = "Cafe", description = "cafe api")
 @RestController
@@ -25,4 +30,20 @@ public class CafeController {
         cafeService.createCafe(cafe);
         return new CommonResponse<>("new cafe 생성에 성공하였습니다.");
     }
+
+    @Operation(summary = "추천 카페 조회")
+    @GetMapping("/recommendation")
+    public CommonResponse<List<CafeRecommendationListResponseDto>> getRecommendations() {
+        List<CafeRecommendationListResponseDto> recommendationList = cafeService.getRecommendationCafes();
+        return new CommonResponse<>(recommendationList, "추천 카페 조회에 성공하였습니다.");
+    }
+
+    @Operation(summary = "추천 카페 조회")
+    @GetMapping("/new")
+    public CommonResponse<List<CafeNewOpenListResponseDto>> getOpenCafes() {
+        List<CafeNewOpenListResponseDto> openCafeList = cafeService.getNewOpenCafes();
+        return new CommonResponse<>(openCafeList, "새로 오픈 카페 조회에 성공하였습니다.");
+    }
+
+
 }
