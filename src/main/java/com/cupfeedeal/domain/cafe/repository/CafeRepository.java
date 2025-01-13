a55a35b3fad5719b80725fe2e324552c1aa50a3d
limgahyun <limgh2002@gmail.com>
2025-01-13 13:31:56 +0900

    feat: findByNameContaining repository

--- src/main/java/com/cupfeedeal/domain/cafe/repository/CafeRepository.java
+++ src/main/java/com/cupfeedeal/domain/cafe/repository/CafeRepository.java
@@ -2,6 +2,8 @@ package com.cupfeedeal.domain.cafe.repository;
 
 import com.cupfeedeal.domain.cafe.entity.Cafe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +12,8 @@ import java.util.List;
 public interface CafeRepository extends JpaRepository<Cafe, Long> {
     List<Cafe> findTop7ByIsRecommendedIsTrueOrderByCreatedAtDesc();
     List<Cafe> findTop7ByIsNewOpenIsTrueOrderByCreatedAtDesc();
+
+    @Query("SELECT c FROM Cafe c WHERE c.name LIKE %:name% and c.deletedAt is null")
+    List<Cafe> findByNameContaining(@Param("name") String name);
 
 }
