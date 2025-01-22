7e93b01719f30dc6afb80b8d915df1db090faf47
limgahyun <limgh2002@gmail.com>
2025-01-22 21:39:53 +0900

    fix: cafe subscription info dto 수정

--- src/main/java/com/cupfeedeal/domain/cafeSubscriptionType/dto/response/CafeSubscriptionInfoResponseDto.java
+++ src/main/java/com/cupfeedeal/domain/cafeSubscriptionType/dto/response/CafeSubscriptionInfoResponseDto.java
@@ -8,12 +8,16 @@ import java.util.List;
 public record CafeSubscriptionInfoResponseDto (
         Long cafe_id,
         String cafe_name,
+        String menu,
+        List<Integer> periods,
         List<CafeSubscriptionListResponseDto> cafe_subscriptions
 ){
     public static CafeSubscriptionInfoResponseDto from(Cafe cafe, List<CafeSubscriptionListResponseDto> cafe_subscriptions) {
         return new CafeSubscriptionInfoResponseDto(
                 cafe.getId(),
                 cafe.getName(),
+                "아이스 아메리카노",
+                List.of(2, 4),
                 cafe_subscriptions
         );
     }
